bc4de42fbec810b602788009afc20fa0c2c6e3ec
kazumatu981 <kazumatu981@gmail.com>
2026-09-05 00:43:59 +0900

    just backup.

--- __tests__/functions/assert.test.js
+++ __tests__/functions/assert.test.js
@@ -1,4 +1,5 @@
-import { describe, it, before, after, mock } from 'node:test';
+import { describe, it, beforeEach, afterEach, mock } from 'node:test';
+import assert from 'node:assert/strict';
 import {
     __assertIsString,
     __assertIsNumber,
@@ -6,142 +7,166 @@ import {
     __assertBetween,
     __assertSomeOf,
     __safeGetElementById,
-    AssertionError,
 } from '../../lib/assert.js';
 
 import { GlobalDocumentMock } from './global-document-mock.js';
 
-function __assertIsInstanceOfAssertionError(error) {
-    if (!(error instanceof AssertionError)) {
-        throw new Error('Expected an instance of AssertionError');
-    }
-}
 describe('__assertIsString', () => {
     it('should not throw an error when the value is a string', () => {
         __assertIsString('hello');
+        assert.ok(true, 'Expected no error to be thrown');
     });
 
     it('should throw an error when the value is not a string', () => {
-        try {
-            __assertIsString(42);
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __assertIsString(42);
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
 });
 
 describe('__assertIsNumber', () => {
     it('should not throw an error when the value is a number', () => {
         __assertIsNumber(42);
+        assert.ok(true, 'Expected no error to be thrown');
     });
 
     it('should throw an error when the value is not a number', () => {
-        try {
-            __assertIsNumber('hello');
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __assertIsNumber('hello');
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
 });
 
 describe('__assertIsFunction', () => {
     it('should not throw an error when the value is an arrow function', () => {
         __assertIsFunction(() => {});
+        assert.ok(true, 'Expected no error to be thrown');
     });
     it('should not throw an error when the value is an anonymous function', () => {
         __assertIsFunction(function () {});
+        assert.ok(true, 'Expected no error to be thrown');
     });
     it('should not throw an error when the value is a named function', () => {
         __assertIsFunction(function myFunction() {});
+        assert.ok(true, 'Expected no error to be thrown');
     });
     it('should not throw an error when the value is a class', () => {
         __assertIsFunction(class MyClass {});
+        assert.ok(true, 'Expected no error to be thrown');
     });
     it('should throw an error when the value is not a function', () => {
-        try {
-            __assertIsFunction(42);
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __assertIsFunction(42);
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
 });
 
 describe('__assertBetween', () => {
     it('should not throw an error when the value is within the range', () => {
         __assertBetween(5, 1, 10);
+        assert.ok(true, 'Expected no error to be thrown');
     });
     it('should not throw an error when the value is equal to the minimum', () => {
         __assertBetween(1, 1, 10);
+        assert.ok(true, 'Expected no error to be thrown');
     });
     it('should not throw an error when the value is equal to the maximum', () => {
         __assertBetween(10, 1, 10);
+        assert.ok(true, 'Expected no error to be thrown');
     });
 
     it('should throw an error when the value is below the range', () => {
-        try {
-            __assertBetween(0, 1, 10);
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __assertBetween(0, 1, 10);
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
     it('should throw an error when the value is above the range', () => {
-        try {
-            __assertBetween(11, 1, 10);
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __assertBetween(11, 1, 10);
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
     it('should throw an error when the value is not a number', () => {
-        try {
-            __assertBetween('hello', 1, 10);
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __assertBetween('hello', 1, 10);
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
     it('should throw an error when the minimum is not a number', () => {
-        try {
-            __assertBetween(5, 'hello', 10);
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __assertBetween(5, 'hello', 10);
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
     it('should throw an error when the maximum is not a number', () => {
-        try {
-            __assertBetween(5, 1, 'hello');
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __assertBetween(5, 1, 'hello');
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
     it('should throw an error when string comparison is used', () => {
-        try {
-            __assertBetween('x', 'a', 'z');
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __assertBetween('x', 'a', 'z');
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
 });
 
 describe('__assertSomeOf', () => {
     it('should not throw an error when the value is in the array', () => {
         __assertSomeOf('a', ['a', 'b', 'c']);
+        assert.ok(true, 'Expected no error to be thrown');
     });
 
     it('should throw an error when the value is not in the array', () => {
-        try {
-            __assertSomeOf('x', ['a', 'b', 'c']);
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __assertSomeOf('x', ['a', 'b', 'c']);
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
 });
 
@@ -156,38 +181,41 @@ describe('__safeGetElementById', () => {
         return null;
     });
 
-    before(() => {
+    beforeEach(() => {
         mockDocument.mock({
             getElementById: mockOfGetElementById,
         });
     });
 
-    after(() => {
+    afterEach(() => {
         mockDocument.unmock();
     });
 
     it('should return the element when it exists', () => {
         const element = __safeGetElementById(testId);
-        if (element !== testElement) {
-            throw new Error(
-                'Expected the returned element to be the test element'
-            );
-        }
+        assert.ok(
+            element === testElement,
+            'Expected the returned element to be the test element'
+        );
     });
     it('should throw an error when the element does not exist', () => {
-        try {
-            __safeGetElementById('non-existent-id');
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __safeGetElementById('non-existent-id');
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
     it('should throw an error when the id is not a string', () => {
-        try {
-            __safeGetElementById(42);
-            throw new Error('Expected an error to be thrown');
-        } catch (error) {
-            __assertIsInstanceOfAssertionError(error);
-        }
+        assert.throws(
+            () => {
+                __safeGetElementById(42);
+            },
+            {
+                name: 'AssertionError',
+            }
+        );
     });
 });
